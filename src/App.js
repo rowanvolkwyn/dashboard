233fe4ad335025941f831f7a62ba7eff41de05b3
Rowan <volkwyn.rowan@gmail.com>
2023-03-27 11:54:52 +1100

    added ability to close the side nav bar

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import { Navbar, Footer, Sidebar, ThemeSettings } from './components/index';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from './pages/index';
 import './App.css';
 
+import { useStateContext } from './contexts/ContextProvider';
+
 const App = () => {
-    const activeMenu = true;
+    const { activeMenu } = useStateContext();
     return (
         <div>
             <BrowserRouter>
