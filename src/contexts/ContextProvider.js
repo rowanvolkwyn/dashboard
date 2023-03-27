174e0b3c1f4015a011b7d636059967a75bc4aed4
Rowan <volkwyn.rowan@gmail.com>
2023-03-27 18:57:27 +1100

    created nav bar

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,12 +11,21 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
+    const [screenSize, setScreenSize] = useState(undefined);
+
+    const handleClick = (clicked) => {
+        setIsClicked({ ...initialState, [clicked]: true});
+    }
 
     return (
         <StateContext.Provider 
             value={{
                 activeMenu: activeMenu,
                 setActiveMenu: setActiveMenu,
+                isClicked: isClicked,
+                setIsClicked: setIsClicked,
+                handleClick: handleClick,
             }}
         >
             {children}
